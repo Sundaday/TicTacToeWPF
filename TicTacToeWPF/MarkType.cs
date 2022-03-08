e9c83a8d35f6fa580300a8bd33766600c563f3a8
Sundaday <john.koch.pro@gmail.com>
2022-03-08 17:07:13 +0100

    one shoot

---
+++ TicTacToeWPF/MarkType.cs
@@ -0,0 +1,21 @@
+﻿namespace TicTacToeWPF
+{
+    /// <summary>
+    /// The type of value a celle in the game is currently at
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// The cell hasn't been clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// The cell is a 0
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// The cell is an X
+        /// </summary>
+        Cross
+    }
+}
